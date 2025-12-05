8b840e552e561bab267d95bc9320099836a2afab
deniska <clearpiece86@gmail.com>
2025-12-05 22:36:23 +0700

    временные компоненты для роутера

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <LabeledInput
+            label="Лейбл первого инпута"
+            placeholder="Текстовый инпут с лейблом"
+        />
+        <LabeledInput
+            label="Лейбл второго инпута"
+            placeholder="Текстовый инпут с лейблом"
+        />
+        <LabeledInput
+            label="Лейбл третьего инпута"
+            placeholder="Текстовый инпут с лейблом"
+        />
+        <ThemeToggleButton />
+        <AppLink
+            href="https://youtube.com"
+            target="_blank"
+            size="small"
+        >
+            Маленькая ссылка на Youtube
+        </AppLink>
+        <AppLink to="/login">Стандартная ссылка на внутреннюю страницу логина</AppLink>
+        <AppLink
+            href="https://google.com"
+            target="_blank"
+            size="large"
+        >
+            Большая ссылка на Google
+        </AppLink>
+    </div>
+</template>
+
+<script setup lang="ts">
+import AppLink from '../components/molecules/AppLink.vue';
+import LabeledInput from '../components/molecules/LabeledInput.vue';
+import ThemeToggleButton from '../components/molecules/ThemeToggleButton.vue';
+</script>
+
+<style scoped>
+div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1em;
+    height: 100%;
+}
+</style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        <div>Страница логина</div>
+        <AppLink to="/">Вернуться обратно</AppLink>
+    </section>
+</template>
+
+<script setup lang="ts">
+import AppLink from '../components/molecules/AppLink.vue';
+</script>
+
+<style scoped>
+section {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+}
+</style>

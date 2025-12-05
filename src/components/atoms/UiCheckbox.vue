acdf5466d54ee05c7f8e977d297f06ced4521c7c
deniska <clearpiece86@gmail.com>
2025-12-05 23:20:33 +0700

    добавил атом чекбокса

---
+++ src/components/atoms/UiCheckbox.vue
@@ -0,0 +1,23 @@
+<template>
+    <input type="checkbox" />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+input {
+    cursor: pointer;
+    accent-color: var(--color-accent);
+}
+
+/* фокус-обводка под твою тему */
+input:focus-visible {
+    outline: 2px solid var(--color-accent);
+    outline-offset: 2px;
+}
+
+/* не обязательно, но можно слегка увеличить чекбокс */
+input {
+    transform: scale(1.75);
+}
+</style>

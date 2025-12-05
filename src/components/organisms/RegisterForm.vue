c828df41565b1fe498a2b405a108b298c3edcd7d
deniska <clearpiece86@gmail.com>
2025-12-06 03:43:21 +0700

    добавил первый организм - форма регистрации

---
+++ src/components/organisms/RegisterForm.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="registration-form">
+        <legend>Регистрация</legend>
+        <section class="inputs-section">
+            <LabeledInput
+                label="Email"
+                type="email"
+                placeholder="cool-email@gmail.com"
+                v-model="emailInput"
+            />
+            <LabeledInput
+                label="Password"
+                type="password"
+                placeholder="cool-password-54"
+                v-model="passwordInput"
+            />
+        </section>
+        <UiButton
+            size="m"
+            @click="register"
+            >Зарегистрироваться</UiButton
+        >
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import UiButton from '../atoms/UIButton.vue';
+import LabeledInput from '../molecules/LabeledInput.vue';
+import { useAuth } from '../../composables/useAuth';
+
+const { registerUser } = useAuth();
+const emailInput = ref<string>('');
+const passwordInput = ref<string>('');
+
+function register() {
+    registerUser(emailInput.value, passwordInput.value);
+}
+</script>
+
+<style scoped>
+.registration-form {
+    width: 25%;
+    height: 70%;
+    padding: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    gap: 1rem;
+
+    background-color: var(--color-bg-secondary);
+    border-radius: 3rem;
+    box-shadow: 1px lightgray;
+}
+
+legend {
+    font-size: 2.5rem;
+}
+
+.inputs-section {
+    display: flex;
+    flex-direction: column;
+    gap: 0.875rem;
+}
+</style>

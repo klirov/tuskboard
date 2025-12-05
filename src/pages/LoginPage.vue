a6ff8d16b3c819083b6a40b773db41a783dd6ae4
deniska <clearpiece86@gmail.com>
2025-12-06 03:43:43 +0700

    тестирую

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,22 +1,20 @@
 <template>
-    <section>
-        <div>Страница логина</div>
-        <AppLink to="/">Вернуться обратно</AppLink>
-    </section>
+    <div>
+        <RegisterForm />
+    </div>
 </template>
 
 <script setup lang="ts">
-import AppLink from '../components/molecules/AppLink.vue';
+import RegisterForm from '../components/organisms/RegisterForm.vue';
 </script>
 
 <style scoped>
-section {
+div {
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    gap: 2rem;
 }
 </style>

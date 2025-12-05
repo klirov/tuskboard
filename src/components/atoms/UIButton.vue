e60c294fafc02bad16423943565be511b4ae23d2
deniska <clearpiece86@gmail.com>
2025-12-06 03:42:33 +0700

    добавил размеры кнопок через пропсы

--- src/components/atoms/UIButton.vue
+++ src/components/atoms/UIButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="transition-theme"
+        :class="['transition-theme', `button--${props.size}`]"
         type="button"
         @click="onClick"
     >
@@ -16,6 +16,15 @@ const emit = defineEmits<{
 function onClick(event: MouseEvent) {
     emit('click', event);
 }
+
+const props = withDefaults(
+    defineProps<{
+        size?: 'xs' | 's' | 'm' | 'l' | 'xl';
+    }>(),
+    {
+        size: 'm',
+    },
+);
 </script>
 
 <style scoped>
@@ -23,10 +32,25 @@ button {
     cursor: pointer;
     width: max-content;
     height: max-content;
-    padding: 0.75em 1em;
+    padding: 0.75rem 1rem;
     border: none;
     background-color: var(--color-accent);
     color: var(--color-text);
     border-radius: 1em;
+}
+.button--xs {
+    font-size: 0.5rem;
+}
+.button--s {
+    font-size: 0.75rem;
+}
+.button--m {
+    font-size: 1rem;
+}
+.button--l {
+    font-size: 1.25rem;
+}
+.button--xl {
+    font-size: 1.5rem;
 }
 </style>

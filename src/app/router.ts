a399d3c24ef2e8eb8efd54cf897b1bc4f4cf582a
deniska <clearpiece86@gmail.com>
2025-12-07 22:23:10 +0700

    добавил страницу логина в роутер

--- src/app/router.ts
+++ src/app/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import LoginPage from '../pages/LoginPage.vue';
+import RegisterPage from '../pages/RegisterPage.vue';
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/login', component: LoginPage },
+    { path: '/register', component: RegisterPage },
 ];
 
 export const router = createRouter({

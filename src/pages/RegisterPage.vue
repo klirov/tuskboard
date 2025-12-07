e3697e881e5515b68bcc424db400cc9478dbc702
deniska <clearpiece86@gmail.com>
2025-12-07 22:24:12 +0700

    создал страницу регистрации

---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <RegisterForm />
+    </div>
+</template>
+
+<script setup lang="ts">
+import RegisterForm from '../components/organisms/RegisterForm.vue';
+</script>
+
+<style scoped>
+div {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>

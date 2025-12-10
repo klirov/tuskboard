a2f89af2e46b3a307f37802595c7115ee87de205
deniska <clearpiece86@gmail.com>
2025-12-11 02:44:31 +0700

    переписал композабл авторизации с использованием обертки апи запросов и ошибками инпутов

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,20 +1,66 @@
-export function useAuth() {
-    async function registerUser(email: string, password: string) {
-        const data = { email, password };
-        const response = await fetch('http://localhost:3000/users', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+import { storeToRefs } from 'pinia';
+import { useUserStore, type User } from '../stores/useUserStore';
+import { useRouter } from 'vue-router';
+import { useApi, type Field } from './useApi';
 
-        // ! Переделать обработку ошибок
-        if (!response.ok) {
-            const error = await response.json().catch(() => null);
-            console.log(error ?? new Error(`HTTP ${response.status}`));
+const URL = 'http://localhost:3000';
+
+export function useAuth() {
+    const $userStore = useUserStore();
+    const { user } = storeToRefs($userStore);
+    const { fieldErrors, requestApi } = useApi();
+    const router = useRouter();
+
+    async function registerUser(
+        email: string,
+        password: string,
+    ): Promise<Partial<Record<Field, string>> | null> {
+        try {
+            const data = await requestApi<User>(`${URL}/register`, {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (data) {
+                user.value = data;
+                router.push('/profile');
+            }
+            return null;
+        } catch (e) {
+            return fieldErrors.value ?? null;
         }
-        return response.json();
     }
-    return { registerUser };
+
+    async function verifyUser(
+        email: string,
+        password: string,
+    ): Promise<Partial<Record<Field, string>> | null> {
+        try {
+            const data = await requestApi<User>(`${URL}/login`, {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+            });
+            if (data) {
+                user.value = data;
+                router.push('/profile');
+            }
+            return null;
+        } catch (e) {
+            return fieldErrors.value ?? null;
+        }
+    }
+
+    async function fetchMe() {
+        const data = await requestApi<User>(`${URL}/protected/me`);
+        user.value = data;
+    }
+
+    async function logout() {
+        await requestApi(`${URL}/logout`, {
+            method: 'POST',
+        });
+        user.value = null;
+    }
+
+    return { registerUser, verifyUser, fetchMe, logout };
 }

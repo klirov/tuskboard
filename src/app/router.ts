050c1897181e693b6de4fc1725336b9318f4f23a
deniska <clearpiece86@gmail.com>
2025-12-11 02:41:53 +0700

    добавил страницу профиля в роутер (пока мок)

--- src/app/router.ts
+++ src/app/router.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/HomePage.vue';
 import LoginPage from '../pages/LoginPage.vue';
 import RegisterPage from '../pages/RegisterPage.vue';
+import ProfilePage from '../pages/ProfilePage.vue';
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/login', component: LoginPage },
     { path: '/register', component: RegisterPage },
+    { path: '/profile', component: ProfilePage },
 ];
 
 export const router = createRouter({

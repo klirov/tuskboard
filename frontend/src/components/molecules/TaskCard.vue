ca2a7e033c9c6ca8a8986b6bdc3b9236fa8bd3dc
deniska <clearpiece86@gmail.com>
2025-12-23 20:11:57 +0700

    создал карточку таска

--- frontend/src/components/molecules/TaskCard.vue
+++ frontend/src/components/molecules/TaskCard.vue
@@ -1,7 +1,46 @@
 <template>
-    <div></div>
+    <div
+        class="task-card"
+        :style="{ 'background-color': cardBackground }"
+    >
+        <h3>{{ task.title }}</h3>
+        <p v-if="task.description">{{ task.description }}</p>
+        <div
+            class="tags"
+            v-if="task.tags?.length"
+        >
+            Tags:
+            <TaskTag
+                v-for="(tag, index) in task?.tags"
+                :key="index"
+                :tag
+            />
+        </div>
+        <small>Created: {{ task.created_at }}</small>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { Task } from '../../../../shared/types';
+import { makeHueFromId } from '../../utils/CardColor';
+import TaskTag from '../atoms/TaskTag.vue';
 
-<style scoped></style>
+const props = defineProps<{ task: Task }>();
+
+const cardBackground = computed<string>(() => {
+    const hue = makeHueFromId(props.task.id);
+    return `hsl(${hue}, 70%, 85%)`;
+});
+</script>
+
+<style scoped>
+.task-card {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    padding: 1rem;
+    border-radius: 0.5rem;
+}
+</style>

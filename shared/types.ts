7e4c19bd8f7420ffdb88017b7a01dc3bca523b6e
deniska <clearpiece86@gmail.com>
2025-12-23 20:14:33 +0700

    форматирование

--- shared/types.ts
+++ shared/types.ts
@@ -1,42 +1,44 @@
 export type AppEnv = {
-  Variables: {
-    user: PublicUser;
-  };
+    Variables: {
+        user: PublicUser;
+    };
 };
 
 export type User = {
-  id: number;
-  email: string;
-  password_hash: string;
-  created_at: Date;
+    id: number;
+    email: string;
+    password_hash: string;
+    created_at: Date;
 };
 
-export type PublicUser = Omit<User, "password_hash">;
+export type PublicUser = Omit<User, 'password_hash'>;
 
-export type Status = "to-do" | "in-progress" | "awaiting" | "done";
+export type Status = 'to-do' | 'in-progress' | 'awaiting' | 'done';
 
 export type Task = {
-  id: number;
-  user_id: number;
-  title: string;
-  description?: string;
-  tags?: string[];
-  status: Status;
+    id: number;
+    user_id: number;
+    title: string;
+    description?: string;
+    tags?: string[];
+    order?: number;
+    status: Status;
+    created_at: string;
 };
 
 export type RegisterIncomingData = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 };
 
 export type LoginIncomingData = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 };
 
 export type ApiResponse<T> = {
-  ok: boolean;
-  status: number;
-  data: T;
-  message?: string;
+    ok: boolean;
+    status: number;
+    data: T;
+    message?: string;
 };

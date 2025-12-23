8bf8e1710d359cb72ede114199f2fc7a8a7bcb32
deniska <clearpiece86@gmail.com>
2025-12-23 20:10:49 +0700

    зарегистрировал роуты

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,24 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { registerUserRoutes } from "./routes/users";
-import { registerAuthRoutes } from "./routes/auth";
-import { AppEnv } from "../../shared/types";
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { registerUserRoutes } from './routes/users';
+import { registerAuthRoutes } from './routes/auth';
+import { AppEnv } from '../../shared/types';
+import { registerTasksRoutes } from './routes/tasks';
 
 const app = new Hono<AppEnv>();
 
 app.use(
-    "*",
+    '*',
     cors({
-        origin: "http://localhost:5173",
-        allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowHeaders: ["Content-Type", "Authorization"],
+        origin: 'http://localhost:5173',
+        allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
-    })
+    }),
 );
 
 registerUserRoutes(app);
 registerAuthRoutes(app);
+registerTasksRoutes(app);
 
 export default app;

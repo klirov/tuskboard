5aa1cc2a0f1d123f37bcc47973601cc08a7304db
deniska <clearpiece86@gmail.com>
2025-12-23 20:13:25 +0700

    создал компонент тега для отображения в карточках

---
+++ frontend/src/components/atoms/TaskTag.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+        class="transition-color"
+        :style="{ 'background-color': tagColor(tag) }"
+    >
+        <p>{{ tag }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { tagColor } from '../../utils/GenerateCardColors';
+
+defineProps<{
+    tag: string;
+}>();
+</script>
+
+<style scoped>
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 0.25rem;
+    border-radius: 0.25rem;
+
+    color: var(--color-text);
+}
+p {
+    font-size: 0.875rem;
+}
+</style>

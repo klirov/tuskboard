38cff0663bc53e4d917b14ca6b37694d343471c1
deniska <clearpiece86@gmail.com>
2025-12-23 20:14:18 +0700

    написал стор для использования и хранения тасков юзера

--- frontend/src/stores/useTasksStore.ts
+++ frontend/src/stores/useTasksStore.ts
@@ -1,11 +1,32 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import { type Task } from '../../../shared/types'
+import { type Task } from '../../../shared/types';
+
+const URL = 'http://localhost:3000';
 
 export const useTasksStore = defineStore('tasks', () => {
     const tasks = ref<Task[]>([]);
+    const loading = ref<boolean>(false);
+
+    async function getUserTasks(userId: number) {
+        loading.value = true;
+
+        try {
+            const response = await fetch(`${URL}/${userId}`);
+            if (response.ok) {
+                const data = await response.json();
+                tasks.value = data.data;
+            }
+        } catch {
+            throw new Error('Failed to fetch tasks');
+        } finally {
+            loading.value = false;
+        }
+    }
 
     return {
         tasks,
+        loading,
+        getUserTasks
     };
 });

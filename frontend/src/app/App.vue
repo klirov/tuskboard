622b2115458fd288cb28fc88606df53eed6c5c75
deniska <clearpiece86@gmail.com>
2025-12-28 18:21:30 +0700

    убрал уведомления, пока не нужны

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,11 +1,9 @@
 <template>
     <RouterView />
-    <NotificationsContainer />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import NotificationsContainer from '../components/molecules/NotificationsContainer.vue';
 </script>
 
 <style scoped></style>

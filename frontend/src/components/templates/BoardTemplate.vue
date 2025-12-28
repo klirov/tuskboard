b325b3d0ef4ea90ea9c125dcbfaf4ed8da3b749c
deniska <clearpiece86@gmail.com>
2025-12-29 02:32:43 +0700

    добавил шаблон для доски чтобы подставлять нужные колонки через слот

---
+++ frontend/src/components/templates/BoardTemplate.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="columns">
+        <slot name="columns" />
+    </div>
+</template>
+
+<style scoped>
+.columns {
+    display: flex;
+    justify-content: center;
+    align-items: start;
+
+    gap: 1rem;
+
+    padding: 1rem;
+}
+</style>

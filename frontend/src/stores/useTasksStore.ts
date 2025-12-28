9f396e2abde7df6d06c5736309be2fd8ff2e1ffc
deniska <clearpiece86@gmail.com>
2025-12-28 18:26:55 +0700

    добавил в стор функционал удаления тасков, открытия и закрытия панели редактирования

--- frontend/src/stores/useTasksStore.ts
+++ frontend/src/stores/useTasksStore.ts
@@ -1,27 +1,27 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { ref, shallowRef } from 'vue';
 import type { Status, Task } from '../../../shared/types';
+import { useApi } from '../composables/useApi';
 
 const URL = 'http://localhost:3000';
 
 export const useTasksStore = defineStore('tasks', () => {
+    const { requestApi, loading } = useApi();
+
     const tasksByStatus = ref<Record<Status, Task[]>>({
+        backlog: [],
         'to-do': [],
         'in-progress': [],
-        'awaiting': [],
-        'done': [],
+        awaiting: [],
+        done: [],
     });
-    const loading = ref<boolean>(false);
 
     async function getUserTasks(userId: number) {
-        loading.value = true;
-
         try {
-            const response = await fetch(`${URL}/tasks/${userId}`);
-            if (response.ok) {
-                const res = await response.json();
-                const groupedTasks = res.data.reduce(
-                    (acc: Record<Status, Task[]>, task: Task) => {
+            const data = await requestApi<Task[]>(`${URL}/tasks/${userId}`);
+            if (data) {
+                const groupedTasks = data.reduce(
+                    (acc, task) => {
                         const status = task.status || 'to-do';
                         acc[status] = acc[status] || [];
                         acc[status].push(task);
@@ -31,16 +31,54 @@ export const useTasksStore = defineStore('tasks', () => {
                 );
                 tasksByStatus.value = groupedTasks;
             }
-        } catch {
-            throw new Error('Failed to fetch tasks');
-        } finally {
-            loading.value = false;
+        } catch (error) {
+            console.error('Error fetching tasks:', error);
         }
+    }
+
+    async function deleteTask(taskId: number) {
+        try {
+            await requestApi(`${URL}/tasks/${taskId}`, {
+                method: 'DELETE',
+            });
+        } catch (error) {
+            console.error('Error deleting task:', error);
+        }
+    }
+
+    async function editTask(taskId: number, taskData: Partial<Task>) {
+        try {
+            await requestApi(`${URL}/tasks/${taskId}`, {
+                method: 'PUT',
+                body: JSON.stringify(taskData),
+            });
+        } catch (error) {
+            throw new Error('Error editing task');
+        }
+    }
+
+    const editingTask = shallowRef<Task | null>(null);
+    const isManagingTask = ref<boolean>(false);
+
+    function openTaskManager(task: Task) {
+        editingTask.value = task;
+        isManagingTask.value = true;
+    }
+
+    function closeTaskManager() {
+        editingTask.value = null;
+        isManagingTask.value = false;
     }
 
     return {
         tasksByStatus,
         loading,
         getUserTasks,
+        deleteTask,
+        editTask,
+        editingTask,
+        isManagingTask,
+        openTaskManager,
+        closeTaskManager
     };
 });

6ab3b7429b16f189840c53172eaaf710a5b260c7
deniska <clearpiece86@gmail.com>
2026-01-16 15:32:02 +0700

    добавил кнопку создания модалки для создания таска

--- frontend/src/components/organisms/headers/BoardHeader.vue
+++ frontend/src/components/organisms/headers/BoardHeader.vue
@@ -1,7 +1,9 @@
 <template>
     <HeaderTemplate>
         <template #left-section></template>
-        <template #center-section></template>
+        <template #center-section>
+            <UiButton @click="emit('task:create')">{{ t('task.create-new-task') }}</UiButton>
+        </template>
         <template #right-section>
             <SwitchLanguageButton /> <ThemeToggleButton /> <ProfileButton />
         </template>
@@ -9,8 +11,16 @@
 </template>
 
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import UiButton from '../../atoms/UiButton.vue';
 import HeaderTemplate from '../../molecules/HeaderTemplate.vue';
 import ProfileButton from '../../molecules/ProfileButton.vue';
 import SwitchLanguageButton from '../../molecules/SwitchLanguageButton.vue';
 import ThemeToggleButton from '../../molecules/ThemeToggleButton.vue';
+
+const { t } = useI18n();
+
+const emit = defineEmits<{
+    (e: 'task:create'): void;
+}>();
 </script>

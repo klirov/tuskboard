92c10fae9eaed0c056c0c295fbd1dcc8aab533b4
deniska <clearpiece86@gmail.com>
2026-01-16 15:29:34 +0700

    создал шаблон для вставки флагов

---
+++ frontend/src/components/atoms/icons/FlagIcon.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg
+        v-if="src"
+        :class="className"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 640 480"
+        :style="{ width: size || '1.5rem', height: size ? `calc(${size} * 0.75)` : '1rem' }"
+    >
+        <image
+            :href="src"
+            width="640"
+            height="480"
+        />
+    </svg>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    src?: string;
+    className?: string;
+    size?: string;
+}>();
+</script>
+
+<style scoped>
+svg {
+    width: 1.5rem;
+    height: 1rem;
+    object-fit: cover;
+}
+</style>

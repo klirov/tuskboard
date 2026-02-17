c7272779cf48f46106d6244a8f24b9a22b6eca82
deniska <clearpiece86@gmail.com>
2026-02-17 16:57:58 +0700

    небольшой фикс по типам

--- frontend/src/utils/GenerateCardColors.ts
+++ frontend/src/utils/GenerateCardColors.ts
@@ -1,4 +1,4 @@
-export function stringHash(str: string) {
+export function stringHash(str: string): number {
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
         hash = str.charCodeAt(i) + ((hash << 5) - hash);
